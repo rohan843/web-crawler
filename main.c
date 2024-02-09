ad2e0e88def3c65807e6fd04e00e0f39e9da50de
rohan843 <rohan.sharma2934@gmail.com>
2024-02-09 20:29:58 +0530

    fix: merge functionality with main

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+
+FILE *filePointer = NULL;
+
+#include "lex.yy.c"
+
+int main()
+{
+    system("curl -o input.txt https://example.com");
+    yyrestart(fopen("input.txt", "r"));
+
+    filePointer = fopen("output.txt", "w");
+    if (filePointer == NULL)
+    {
+        printf("Error opening the file.\n");
+        return 1; // Return an error code
+    }
+
+    time_t currentTime;
+    time(&currentTime);
+    char *timeString = ctime(&currentTime);
+
+    // Print the current time
+    fprintf(filePointer, "Current Time: %s", timeString);
+
+    yylex();
+
+    fclose(filePointer);
+    return 0;
+}
